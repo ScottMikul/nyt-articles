0425410ff0d4f1d745c018faad5aee83dc1d592e
scott mikul <scott.p.mikul@gmail.com>
2020-01-11 17:23:12 -0500

    added some javascript to get the elements and add them to the article

---
+++ assets/scripts/script.js
@@ -0,0 +1,33 @@
+ var SearchTerm= "donald%20Trump";
+ var Pages= "10";
+ var BeginDate= "20180101";
+ var EndDate= "20190101";
+ // This is our API key. Add your own API key between the ""
+ var APIKey = "Ii3njx05nbjP142su0k8NmFO7dABjhMp";
+ // Here we are building the URL we need to query the database
+ var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=" + BeginDate + "&end_date=" + EndDate + "&fq=" + SearchTerm + "&page=" + Pages + "&api-key=" + APIKey;
+// We then created an AJAX call
+var resultArray; 
+$.ajax({
+   url: queryURL,
+   method: "GET"
+ }).then(function(response) {
+   // Create CODE HERE to Log the queryURL
+   console.log(response);
+   resultArray = response.response.docs;
+   for(var i=0;i<Pages;i++){
+       var listEl = $("<li>");
+
+       console.log(resultArray[i].headline.main);
+       var articleName = $("<div>");
+       articleName.text(resultArray[i].headline.main);
+       listEl.append(articleName);
+
+       var author = $("<div>");
+       author.text(resultArray[i].byline.original);
+       listEl.append(author);
+
+       $("#articles").append(listEl);
+   }
+
+ });
