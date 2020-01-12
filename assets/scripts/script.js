1883dbccb54379b80222320bc472e2ad2f0379ba
Scotty <Scott.p.mikul@gmail.com>
2020-01-11 19:14:41 -0500

    changed the pages variable to use the selector form

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,57 +1,57 @@
- 
- function clearresults(){
-    $("#articles").empty();
- }
- function ShowHeadline(){
- 
- var SearchT= $("#searchTerm").val();
- var SearchTerm= SearchT.trim();
- console.log(SearchTerm);
-var Pages= "5";
- //  var Pages= $("#form-control").val();
- console.log(Pages)
- var BeginD= $("#startYear").val();
- var BeginDate= BeginD+"0101";
- console.log(BeginDate);
- var EndD= $("#endYear").val();
- var EndDate= EndD+"0101";
- console.log(EndDate);
- 
- 
- // This is our API key. Add your own API key between the ""
- var APIKey = "Ii3njx05nbjP142su0k8NmFO7dABjhMp";
- // Here we are building the URL we need to query the database
- var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=" + BeginDate + "&end_date=" + EndDate + "&fq=" + SearchTerm + "&page=5" + "&api-key=" + APIKey;
 
-
-// We then created an AJAX call
-var resultArray; 
-$.ajax({
-   url: queryURL,
-   method: "GET"
- }).then(function(response) {
-   // Create CODE HERE to Log the queryURL
-console.log(response.response.docs[0]);
-
-
-$("#articles").empty();
-   resultArray = response.response.docs;
-   for(var i=0;i<Pages;i++){
-       var listEl = $("<li>");
-
-       console.log(resultArray[i].headline.main);
-       var articleName = $("<div>");
-       articleName.text(resultArray[i].headline.main);
-       listEl.append(articleName);
-
-       var author = $("<div>");
-       author.text(resultArray[i].byline.original);
-       listEl.append(author);
-
-       $("#articles").append(listEl);
-   }
-
- });
+function clearresults() {
+   $("#articles").empty();
 }
- $("#searchBtn").on("click", ShowHeadline);
- $("#clearBtn").on("click", clearresults);
+function ShowHeadline() {
+
+   var SearchT = $("#searchTerm").val();
+   var SearchTerm = SearchT.trim();
+   console.log(SearchTerm);
+   var Pages = "5";
+   Pages = $("#recordNumber").val()
+   console.log(Pages)
+   var BeginD = $("#startYear").val();
+   var BeginDate = BeginD + "0101";
+   console.log(BeginDate);
+   var EndD = $("#endYear").val();
+   var EndDate = EndD + "0101";
+   console.log(EndDate);
+
+
+   // This is our API key. Add your own API key between the ""
+   var APIKey = "Ii3njx05nbjP142su0k8NmFO7dABjhMp";
+   // Here we are building the URL we need to query the database
+   var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=" + BeginDate + "&end_date=" + EndDate + "&fq=" + SearchTerm + "&page=5" + "&api-key=" + APIKey;
+
+
+   // We then created an AJAX call
+   var resultArray;
+   $.ajax({
+      url: queryURL,
+      method: "GET"
+   }).then(function (response) {
+      // Create CODE HERE to Log the queryURL
+      console.log(response.response.docs[0]);
+
+
+      $("#articles").empty();
+      resultArray = response.response.docs;
+      for (var i = 0; i < Pages; i++) {
+         var listEl = $("<li>");
+
+         console.log(resultArray[i].headline.main);
+         var articleName = $("<div>");
+         articleName.text(resultArray[i].headline.main);
+         listEl.append(articleName);
+
+         var author = $("<div>");
+         author.text(resultArray[i].byline.original);
+         listEl.append(author);
+
+         $("#articles").append(listEl);
+      }
+
+   });
+}
+$("#searchBtn").on("click", ShowHeadline);
+$("#clearBtn").on("click", clearresults);
